073820271c3de34b26bdb7b5aa4a9a818b9b61a4
318Thorne <c3186564@uon.edu.au>
2020-03-15 20:40:29 +1100

    routing with netlify

---
+++ sites/lookup/src/pages/search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Index = () => (
+  <>
+    <h1>Search For Rick & Morty Characters</h1>
+    <p>
+      Trying to remember which rick you are talking about? try out this handy
+      search interface
+    </p>
+    {/* add form */}
+    {/* render results */}
+  </>
+);
+
+export default Index;
